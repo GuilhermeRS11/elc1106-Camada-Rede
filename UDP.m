500c9abaae928b963c9cd4a9a2c88c16b2663d51
GuilhermeRS11 <guilhermeribeiro201342@gmail.com>
2023-07-07 18:38:43 -0600

    Implementação do protocolo UDP

--- UDP.m
+++ UDP.m
@@ -1,139 +1,213 @@
 clc; clear all; close all;
 
-% Inicia o gerador de números aleatórios
+% ---------------------------------------------------------
+% Parâmetro modificável da rede. Efeito no Packet Loss Rate
+n = 20; % numero de nos da rede
+% ---------------------------------------------------------
+
+% Inicia o gerador de numeros aleatorios
 rand("state", 0);
 
-% Parâmetros principais
-tempo_simulacao = 100; % tempo de simulação
+% Parametros principais
+tempo_simulacao = 100; % tempo de simulacao
 
 % Lista de eventos executados
 global Log_eventos = [];
 global eventos_executados = 0;
 
-n = 20; % número de nós da rede
-global msg = "hello";
+global msg = {"hello"};
 global rede = ~eye(n); % matriz de conectividade da rede
 global nos = [];
 
+% Definir a estrutura do cabeçalho do UDP
+global udp_header_size = 8; % Tamanho do cabeçalho UDP em bytes
+
+global total_packets_received = 0;
+global total_packets_sent = 0;
+
+% Função para calcular o checksum do cabeçalho UDP
+function checksum = calculateChecksum(header)
+    % Implemente aqui o cálculo do checksum (opcional para a simulação)
+    checksum = 0;
+end
+
 function Log_eventos = exec_simulador(Lista_eventos, Log_eventos, tempo_final)
+    global eventos_executados;
 
-  global eventos_executados;
+    % Simulacao discreta por eventos
+    while 1
+        [min_instante, min_indice] = min([Lista_eventos(:).instante]);
+        if isempty(min_instante)
+            break;
+        end
+        if min_instante > tempo_final
+            break;
+        end
+        ev = Lista_eventos(min_indice);
+        Lista_eventos(min_indice) = []; % Remove o evento da lista, pois sera executado.
+        tempo_atual = min_instante;
+        Log_eventos = [Log_eventos; ev];
 
-  % Simulação discreta por eventos
-  while 1
-    [min_instante, min_indice] = min([Lista_eventos(:).instante]);
-    if isempty(min_instante)
-      break;
+        Novos_eventos = executa_evento(ev, tempo_atual); % Retorna os novos eventos apos executar o ultimo evento
+        eventos_executados += 1;
+
+        if ~isempty(Novos_eventos) % adiciona novos eventos na lista
+            Lista_eventos = [Lista_eventos; Novos_eventos];
+        end
     end
-    if min_instante > tempo_final
-      break;
-    end
-    ev = Lista_eventos(min_indice);
-    Lista_eventos(min_indice) = []; % Remove o evento da lista, pois será executado.
-    tempo_atual = min_instante;
-    Log_eventos = [Log_eventos; ev];
-
-    Novos_eventos = executa_evento(ev, tempo_atual); % Retorna os novos eventos após executar o último evento
-    eventos_executados += 1;
-
-    if ~isempty(Novos_eventos) % Adiciona novos eventos na lista
-      Lista_eventos = [Lista_eventos; Novos_eventos];
-    end
-  end
 end
 
 function [NovosEventos] = executa_evento(evento, tempo_atual)
-  global msg, global rede, global nos;
+    global msg, global rede, global nos, global udp_header_size;
+    global total_packets_received, global total_packets_sent;
 
-  NovosEventos = [];
+    NovosEventos = [];
 
-  % Configuração
-  dist = 100; % 100m
-  tempo_prop = dist / 3e8; % tempo de propagação = distância/velocidade do sinal
+    % Configuração
+    dist = 100; % 100m
+    tempo_prop = dist / 3e8; %tempo de propagacao = distancia/velocidade do sinal
+    taxa_dados = 1e5; % 100kbps
 
-  [t, tipo_evento, id, pct] = evento_desmonta(evento); % Retorna os campos do 'evento'
-  disp(['EV: ' tipo_evento ' @t=' num2str(t) ' id=' num2str(id)]);
+    [t, tipo_evento, id, pct] = evento_desmonta(evento); % retorna os campos do 'evento'
+    disp(['EV: ' tipo_evento ' @t=' num2str(t) ' id=' num2str(id)]);
 
-  switch tipo_evento
-    case 'N_cfg' % Configura nós, inicia variáveis de estado, etc.
-      nos(id).ocupado_ate = 0;
+    switch tipo_evento
+        case 'N_cfg' % configura nos, inicia variaveis de estado, etc.
+            nos(id).Tx = 'desocupado';
+            nos(id).Rx = 'desocupado';
+            nos(id).ocupado_ate = 0;
+            nos(id).stat = struct("tx", 0, "rx", 0, "rxok", 0, "col", 0);
 
-      % Adiciona um envio na fila
-      pct = struct('src', id, 'dst', 0, 'dados', msg);
+            % adiciona uma trasmissao na fila
+            % pacote contem origem (src), destino (dst), tamanho (tam) e os dados
+            pct = struct('src', id, 'dst', 0, 'tam', 20, 'dados', msg);
 
-      e = evento_monta(tempo_atual + rand(1), 'S_ini', id, pct);
-      NovosEventos = [NovosEventos; e];
+            % Add IP header fields
+            ip_header = struct('source_ip', '192.168.0.1', 'destination_ip', '192.168.0.2');
+            pct.ip_header = ip_header;
 
-    case 'S_ini' % Início do envio
-      if nos(id).ocupado_ate > tempo_atual % Verifica se o nó está ocupado
-        tempo_entre_envios = 0.2; % 200ms
-        e = evento_monta(nos(id).ocupado_ate + tempo_entre_envios, 'S_ini', id, pct);
-        NovosEventos = [NovosEventos; e];
-      else
-        if pct.dst == 0 % Pacote de broadcast
-          for nid = find(rede(id, :) > 0) % Envia uma cópia do pacote para cada vizinho
-            disp(['INI S de ' num2str(id) ' para ' num2str(nid) ' @t=' num2str(tempo_atual)]);
-            e = evento_monta(tempo_atual + tempo_prop, 'S_env', nid, pct);
+            % Add UDP header fields
+            udp_header = struct('source_port', 5000, 'destination_port', 6000, 'length', udp_header_size + pct.tam, 'checksum', 0);
+            pct.udp_header = udp_header;
+
+            e = evento_monta(tempo_atual + rand(1), 'T_ini', id, pct);
             NovosEventos = [NovosEventos; e];
-          end
-        else % Pacote unicast
-          disp(['INI S de ' num2str(id) ' para ' num2str(pct.dst) ' @t=' num2str(tempo_atual)]);
-          e = evento_monta(tempo_atual + tempo_prop, 'S_env', pct.dst, pct);
-          NovosEventos = [NovosEventos; e];
-        end
 
-        nos(id).ocupado_ate = tempo_atual + 0.2; % 200ms
-      end
+        case 'T_ini' %inicio de transmissao
+            if strcmp(nos(id).Tx, 'ocupado') % transmissor ocupado?
+                tempo_entre_quadros = 0.2 * 8 * pct.tam / taxa_dados; %20\% do tempo de transmissao
+                e = evento_monta(nos(id).ocupado_ate + tempo_entre_quadros, 'T_ini', id, pct);
+                NovosEventos = [NovosEventos; e];
+            else
+                if pct.dst == 0 %pacote de broadcast
+                    for nid = find(rede(id, :) > 0) % envia uma copia do pacote para cada vizinho
+                        disp(['INI T de ' num2str(id) ' para ' num2str(nid)]);
+                        e = evento_monta((tempo_atual + tempo_prop), 'R_ini', nid, pct);
+                        NovosEventos = [NovosEventos; e];
+                    end
+                else % envia um pacote para o vizinho, se conectado
+                    if find(rede(id, :) == pct.dst)
+                        disp(['INI T de ' num2str(id) ' para ' num2str(pct.dst)]);
+                        e = evento_monta((tempo_atual + tempo_prop), 'R_ini', pct.dst, pct);
+                        NovosEventos = [NovosEventos; e];
+                    end
+                end
+                tempo_transmissao = 8 * pct.tam / taxa_dados;
+                e = evento_monta((tempo_atual + tempo_transmissao), 'T_fim', id, pct);
+                NovosEventos = [NovosEventos; e];
+                nos(id).Tx = 'ocupado';
+                nos(id).ocupado_ate = tempo_atual + tempo_transmissao;
+            end
+        case 'T_fim' %fim de transmissao
+            nos(id).stat.tx += 1;
+            nos(id).Tx = 'desocupado';
+            nos(id).ocupado_ate = 0;
+        case 'R_ini' %inicio de recepcao
 
-    case 'S_env' % Envio de pacote
-      disp(['ENVIO de ' num2str(id) ' para ' num2str(pct.dst) ' @t=' num2str(tempo_atual)]);
-      if rand(1) > 0.2 % Taxa de perda de pacotes = 20%
-        e = evento_monta(tempo_atual + tempo_prop, 'S_rec', pct.dst, pct);
-        NovosEventos = [NovosEventos; e];
-      else
-        disp('Pacote PERDIDO!');
-      end
+            %if ~isempty(pct); disp(pct); end;
+            if strcmp(nos(id).Rx, 'ocupado') || strcmp(nos(id).Rx, 'colisao')
+                nos(id).Rx = 'colisao';
+                nos(id).stat.rx += 1;
+                disp('--- Pacote perdido ---');
+            else
+                nos(id).Rx = 'ocupado';
+                nos(id).stat.rx = 1;
+            end
+            e = evento_monta((tempo_atual + 8 * pct.tam / taxa_dados), 'R_fim', id, pct);
+            NovosEventos = [NovosEventos; e];
+            total_packets_sent += 1;
 
-    case 'S_rec' % Recepção de pacote
-      disp(['RECEP de ' num2str(id) ' de ' num2str(pct.src) ' @t=' num2str(tempo_atual)]);
+        case 'R_fim' %fim de recepcao
+            nos(id).stat.rx -= 1;
+            if strcmp(nos(id).Rx, 'ocupado')
+                disp("");
+                disp(['FIM R de ' num2str(pct.src) ' para ' num2str(pct.dst)']);
 
-    otherwise
-      disp('Evento não tratado');
-  end
+                disp('--- Pacote recebido ---');
+                total_packets_received += 1;
+
+                 % Print packet details
+                disp(['Origem: ' num2str(pct.src)]);
+                disp(['Destino: ' num2str(pct.dst)]);
+                disp(['Tamanho: ' num2str(pct.tam)]);
+                disp(['Dados: ' num2str(pct.dados)]);
+                disp('IP Header: ');
+                disp(pct.ip_header);
+                disp('UDP Header: ');
+                disp(pct.udp_header);
+            elseif strcmp(nos(id).Rx, 'colisao')
+                if (nos(id).stat.rx == 0)
+                    nos(id).Rx = 'desocupado';
+                    nos(id).stat.col += 1;
+                end
+            else
+                disp("ERRO: Estado Rx errado.");
+            end
+        case 'S_fim' %fim de simulacao
+            disp('Simulacao encerrada!');
+        otherwise
+            disp(['exec_evento: Evento desconhecido: ' tipo_evento]);
+    end
+
 end
 
-function [ev] = evento_monta(instante, tipo, id, pct)
-  ev = struct('instante', instante, 'tipo', tipo, 'id', id, 'pct', pct);
+function [t, tipo, id, pct] = evento_desmonta(e)
+    t = e.instante;
+    tipo = e.tipo;
+    id = e.id;
+    pct = e.pct;
 end
 
-function [t, tipo_evento, id, pct] = evento_desmonta(evento)
-  t = evento.instante;
-  tipo_evento = evento.tipo;
-  id = evento.id;
-  pct = evento.pct;
+function e = evento_monta(t, tipo, id, pct)
+    if nargin < 4, pct = []; end
+    e = struct('instante', t, 'tipo', tipo, 'id', id);
+    e.pct = pct;
 end
 
-% Cria os nós da rede
-for i = 1:n
-  nos(i).id = i;
-  nos(i).ocupado_ate = 0;
+function Lista_eventos = config_sim(n, tempo_simulacao)
+    Lista_eventos = [];
+    for k = 1:n
+        e = evento_monta(0, 'N_cfg', k);
+        Lista_eventos = [Lista_eventos; e];
+    end
+
+    ev_fim = evento_monta(tempo_simulacao, 'S_fim', 0);
+    Lista_eventos = [Lista_eventos; ev_fim];
 end
 
-% Configura eventos iniciais
-Lista_eventos = [];
-for i = 1:n
-  e = evento_monta(0, 'N_cfg', i, []);
-  Lista_eventos = [Lista_eventos; e];
-end
+% Configura a simulacao
+tempo_inicial = clock();
+Lista_eventos = config_sim(n, tempo_simulacao);
 
-% Executa a simulação
+% Executa a simulacao
 Log_eventos = exec_simulador(Lista_eventos, Log_eventos, tempo_simulacao);
+print_struct_array_contents(1);
+% Log_eventos(:).instante
+% Log_eventos(:).tipo
+disp(['---Total de eventos=' num2str(eventos_executados)]);
+disp(sprintf('---Tempo da simulacao=%g segundos', etime(clock, tempo_inicial)));
 
-% Exibe resultados
-disp(['Eventos executados: ' num2str(eventos_executados)]);
-disp('--- Log de Eventos ---');
-for i = 1:size(Log_eventos, 1)
-  [t, tipo_evento, id, pct] = evento_desmonta(Log_eventos(i, :));
-  disp(['@t=' num2str(t) ' EV: ' tipo_evento ' id=' num2str(id)]);
-end
+% Faz o calculo da % de pacotes perdidos
+packet_loss_rate = 1 - (total_packets_received / total_packets_sent);
+disp(['Packet Loss Rate: ' num2str(packet_loss_rate)]);
 
